4cd3039839085f69f7f88f36bee13117e7accc07
bedelman073 <edelmanb@northeastern.edu>
2024-03-16 19:37:39 -0400

    Adding intial features, such as color theme

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,7 +1,11 @@
-# File
-
 import customtkinter as ctk
+from PIL import Image
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("dark-blue")
 window = ctk.CTk()
+window.geometry("600x440")
+window.title("Login")
+header=ctk.CTkLabel(master=window,text="Login to Your Account")
+header.pack()
 window.mainloop()
 
-print("test")
