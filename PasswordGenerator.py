7d83c429b9138dd9adf3696ffca17a2ee3af32d6
bedelman073 <edelmanb@northeastern.edu>
2024-03-17 13:17:05 -0400

    Removed the database feature. Added image

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,23 +1,37 @@
 # Importing modules
 import customtkinter as ctk
-import sqlite3
-import bcrypt
 from PIL import Image
-# initalizing a secure database to store passwords and usernames
-conn=sqlite3.connect("data.db")
-cursor=conn.cursor()
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        username TEXT NOT NULL,
-        password TEXT NOT NULL)''')
+
 # setting fonts
-font1=("Helvetica",25,"bold")
-# creating a window 
+font1 = ("Helvetica", 20, "bold")
+
+# creating a window
 ctk.set_appearance_mode("system")
 ctk.set_default_color_theme("dark-blue")
 window = ctk.CTk()
-window.geometry("450x360")
+window.geometry("550x450")
 window.title("Login")
-header=ctk.CTkLabel(master=window,text="Login to Your Account")
-header.pack()
+
+# Creating frames
+right_frame = ctk.CTkFrame(window,width=275, height=450)
+right_frame.pack(side="right",fill="both",expand=True)
+left_frame = ctk.CTkFrame(window, width=275, height=450)
+left_frame.pack(side="left",fill="both",expand=True)
+
+# Creating and packing header
+header = ctk.CTkLabel(right_frame, text="Secure Password Generator", font=font1)
+header.pack(anchor="n")
+
+# Creating and packing entry widgets
+username_ent = ctk.CTkEntry(right_frame, placeholder_text="Username")
+username_ent.pack(pady=10)
+password_ent = ctk.CTkEntry(right_frame, placeholder_text="Password")
+password_ent.pack()
+submit = ctk.CTkButton(right_frame,width=50,height=10,corner_radius=5)
+submit.pack(pady=30)
+# Creating and packing image
+img1 = ctk.CTkImage(light_image=Image.open(r"C:\Users\Ben Edelman\PasswrdGenerator\menuicon.jpg"), dark_image=Image.open(r"C:\Users\Ben Edelman\PasswrdGenerator\menuicon.jpg"), size=(275, 450))
+img_lab = ctk.CTkLabel(left_frame, text="", image=img1)
+img_lab.pack()
+
 window.mainloop()
